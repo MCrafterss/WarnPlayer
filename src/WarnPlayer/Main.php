0ea5543e7d09b5500768845b714a9a8fa4b0992b
applqpak <applqpak@gmail.com>
2016-02-03 10:27:24 +1100

    Update Main.php

--- src/WarnPlayer/Main.php
+++ src/WarnPlayer/Main.php
@@ -34,6 +34,8 @@
 
           $player = $this->getServer()->getPlayer($name);
 
+          $player_name = $player->getName();
+
           $sender_name = $sender->getName();
 
           $sender_display_name = $sender->getDisplayName();
@@ -52,9 +54,9 @@
 
             $player->sendMessage(TF::RED . "You have been warned by " . $sender_name . " for " . $reason);
 
-            $sender->sendMessage(TF::GREEN . "You have warned " . $name . " for " . $reason);
+            $sender->sendMessage(TF::GREEN . "You have warned " . $player_name . " for " . $reason);
 
-            $this->getServer()->broadcastMessage(TF::YELLOW . $name . " has been warned by " . $sender_name . " for " . $reason);
+            $this->getServer()->broadcastMessage(TF::YELLOW . $player_name . " has been warned by " . $sender_name . " for " . $reason);
 
             return true;
 
