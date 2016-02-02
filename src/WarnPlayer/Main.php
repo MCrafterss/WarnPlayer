2c3d1b3a90e3bc1ef3b9e7c43af625f9fc964406
applqpak <applqpak@gmail.com>
2016-02-02 13:07:16 +1100

    Update Main.php
    FIXED ALL BUGS!!

--- src/WarnPlayer/Main.php
+++ src/WarnPlayer/Main.php
@@ -1,31 +1,70 @@
 <?php
+
   namespace WarnPlayer;
+
   use pocketmine\plugin\PluginBase;
   use pocketmine\event\Listener;
   use pocketmine\utils\TextFormat as TF;
   use pocketmine\command\Command;
   use pocketmine\command\CommandSender;
-  use pocketmine\command\CommandExecutor;
   use pocketmine\Player;
+
   class Main extends PluginBase implements Listener {
+
+
     public function onEnable() {
       $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
     }
+
     public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
-      if($cmd->getName() === "warn") {
+
+      if(strtolower($cmd->getName()) === "warn") {
+
         if(!(isset($args[0]) and isset($args[1]))) {
-          $sender->sendMessage(TF::RED . "Error: not enough args. Usage: /warn <player> <reason>");
+
+          $sender->sendMessage(TF::RED . "Error: not enough args. Usage: /warn <player> < reason >");
+
+          return true;
+
         } else {
+
+          $name = $args[0];
+
+          $player = $this->getServer()->getPlayer($name);
+
           $sender_name = $sender->getName();
-          $player = $this->getServer()->getPlayer($args[0]);
-          $player_name = $player->getName();
+
+          $sender_display_name = $sender->getDisplayName();
+
           unset($args[0]);
+
           $reason = implode(" ", $args);
-          $player->sendMessage(TF::RED . "You have been warned by " . $sender_name . " for " . $reason . "!");
-          $this->getServer()->broadcastMessage(TF::YELLOW . $player_name . " was warned by " . $sender_name . " for " . $reason . "!");
-          $sender->sendMessage(TF::GREEN . $player_name . " was warned for " . $reason . "!");
+
+          if($player === null) {
+
+            $sender->sendMessage(TF::RED . "Player " . $name . " could not be found.");
+
+            return true;
+
+          } else {
+
+            $player->sendMessage(TF::RED . "You have been warned by " . $sender_name . " for " . $reason);
+
+            $sender->sendMessage(TF::GREEN . "You have warned " . $name . " for " . $reason);
+
+            $this->getServer()->broadcastMessage(TF::YELLOW . $name . " has been warned by " . $sender_name . " for " . $reason);
+
+            return true;
+
+          }
+
         }
+
       }
+
     }
+
   }
+
 ?>
