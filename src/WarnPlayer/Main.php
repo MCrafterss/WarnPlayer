ad21484b95c6325e6cca893c1c3f44a55c01f2cd
Driesboy <coenendries3@gmail.com>
2017-03-11 11:03:44 +0100

    Update Main.php

--- src/WarnPlayer/Main.php
+++ src/WarnPlayer/Main.php
@@ -41,32 +41,32 @@
             $reason = implode(" ", $args);
             $file = file_get_contents($this->dataPath() . "Players/" . strtolower($player_name) . ".txt");
             if($file >= "3") {
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 1 1440 warned 3+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 2 1440 warned 3+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 3 1440 warned 3+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 4 1440 warned 3+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 5 1440 warned 3+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 6 1440 warned 3+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 7 1440 warned 3+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 8 1440 warned 3+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 9 1440 warned 3+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 10 1440 warned 3+ times");
-              file_put_contents($this->dataPath() . "Players/" . strtolower($player_name) . ".txt", "0");
-              $sender->sendMessage(TF::GREEN . "Warned " . $player_name . ", and jailed for 1 day.");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 1 10080 warned 3+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 2 10080 warned 3+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 3 10080 warned 3+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 4 10080 warned 3+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 5 10080 warned 3+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 6 10080 warned 3+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 7 10080 warned 3+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 8 10080 warned 3+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 9 10080 warned 3+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "jail " . $player_name . " 10 10080 warned 3+ times");
+              file_put_contents($this->dataPath() . "Players/" . strtolower($player_name) . ".txt", $file = "0");
+              $sender->sendMessage(TF::GREEN . "Warned " . $player_name . ", and jailed for 1 week.");
             }
             if($file >= "2") {
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 1 5 warned 2+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 2 5 warned 2+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 3 5 warned 2+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 4 5 warned 2+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 5 5 warned 2+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 6 5 warned 2+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 7 5 warned 2+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 8 5 warned 2+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 9 5 warned 2+ times");
-              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 10 5 warned 2+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 1 1440 warned 2+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 2 1440 warned 2+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 3 1440 warned 2+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 4 1440 warned 2+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 5 1440 warned 2+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 6 1440 warned 2+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 7 1440 warned 2+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 8 1440 warned 2+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 9 1440 warned 2+ times");
+              $this->getServer()->dispatchCommand(new ConsoleCommandSender(),"jail " . $player_name . " 10 1440 warned 2+ times");
               file_put_contents($this->dataPath() . "Players/" . strtolower($player_name) . ".txt", $file + 1);
-              $sender->sendMessage(TF::GREEN . "Warned " . $player_name . ", and jailed for 5 min.");
+              $sender->sendMessage(TF::GREEN . "Warned " . $player_name . ", and jailed for 1 day.");
             }else{
               $player->sendMessage(TF::YELLOW . "You have been warned by " . $sender_name . " for " . $reason);
               $this->getServer()->broadcastMessage(TF::YELLOW . $player_name . " was warned by " . $sender_name . " for " . $reason);
